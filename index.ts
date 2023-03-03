476c519d48b9c021e8e4e6db8c6e0b24aa9ff447
Christian Gabriel Simon Koch <christian.simon.koch@rwth-aachen.de>
2023-03-03 13:42:05 +0100

    initialize webwriter project

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {LitElementWw} from "@webwriter/lit"
+import {html} from "lit"
+import {customElement} from "lit/decorators.js"
+
+@customElement("ww-machinelearningvisualizer")
+export class WwMachinelearningvisualizer extends LitElementWw {
+  render() {
+    return html`Hello, world!`
+  }
+}
